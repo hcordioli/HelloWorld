026d46450d54a9486fc515b47739882e45c0c5f3
Humberto <hcordioli@gmail.com>
2017-06-27 17:11:03 -0300

    added HelloWorld program

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+/*	My first Java code...
+	How cool is that?
+*/
+	public static void main (String args[]) {
+		System.out.println ("Hi folks! Back on the wagon again... Java is up and running!");
+		for (int i=1; i<=10; i++)
+				System.out.println ("Hi "+i);
+	}
+}
